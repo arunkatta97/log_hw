9b6cd9cd46698145e78ee18685998673ce5be26e
arunkatta97 <arun.katta97@gmail.com>
2022-04-19 15:28:22 -0400

    app refactored

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,11 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
 
 from app.auth import auth
-from app.logging_config import log_con
+from app.auth.user_operation import user_operations
 from app.auth import auth
 from app.cli import create_database, create_log_folder
 from app.context_processors import utility_text_processors
+from app.logging_config import log_con
 from app.db import db
 from app.db.models import User
 from app.exceptions import http_exceptions
@@ -30,18 +31,6 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        if has_request_context():
-            record.url = request.url
-            record.remote_addr = request.remote_addr
-        else:
-            record.url = None
-            record.remote_addr = None
-
-        return super().format(record)
-
-
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
@@ -53,6 +42,7 @@ def create_app():
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
     app.register_blueprint(log_con)
+    app.register_blueprint(user_operations)
     app.context_processor(utility_text_processors)
     app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
     app.register_error_handler(404, page_not_found)
@@ -64,9 +54,6 @@ def create_app():
     # add command function to cli commands
     app.cli.add_command(create_database)
     app.cli.add_command(create_log_folder)
-
-    # Deactivate the default flask logger so that log messages don't get duplicated
-    app.logger.removeHandler(default_handler)
 
     # get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
