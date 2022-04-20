af98d14a60042cfc514323e4d3a1b6c8871eae3c
arunkatta97 <arun.katta97@gmail.com>
2022-04-19 20:17:18 -0400

    app refactored

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,13 +1,14 @@
 """This test the homepage"""
+from tests.conftest import make_log_folder
 
-def test_request_main_menu_links(client,make_log_folder):
+def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
-def test_auth_pages(client,make_log_folder):
+def test_auth_pages(client):
     """This makes the index page"""
     response = client.get("/dashboard")
     assert response.status_code == 302
